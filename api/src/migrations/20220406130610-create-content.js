343921e737d989f7564d098ea25a042842a40fcd
HMtsumura <hironori0811@gmail.com>
2022-04-06 22:07:20 +0900

    created Content table

---
+++ api/src/migrations/20220406130610-create-content.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Contents', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      category_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      title_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      comment: {
+        type: Sequelize.STRING
+      },
+      record_ymd: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Contents');
+  }
+};
---
+++ api/src/models/content.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Content extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Content.init({
+    user_id: DataTypes.INTEGER,
+    category_id: DataTypes.INTEGER,
+    title_id: DataTypes.INTEGER,
+    comment: DataTypes.STRING,
+    record_ymd: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Content',
+  });
+  return Content;
+};
