1bf1d3f890afbaa90c451a0ceaefdf654dfdf5c7
HMtsumura <hironori0811@gmail.com>
2022-04-08 15:30:08 +0900

    installed http-proxy-middleware

--- user/src/front/src/App.tsx
+++ user/src/front/src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const server = 'http://localhost:8088/users';
+
+interface State{
+  result: string 
+}
+
+class App extends Component {
+  state: State ={
+    result: ''
+  }
+  constructor(props: any, state: State) {
+    super(props);
+    this.state = {
+      result: '',
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    axios.get('/users')
+      .then((res) => {
+        this.setState({
+          status: true,
+          result: res.data
+        });
+        console.log(this.state);
+      })
+      .catch((e) => {
+        console.error(e);
+        this.setState({
+          status: false,
+          result: e,
+        });
+      });
+  }
+
+  render() {
+    const result = <div>{this.state.result}</div>
+    return (
+      <div>
+        <button onClick={this.handleClick}>Get Data</button>
+        {result}
+      </div>
+    );
+  }
 }
 
 export default App;
