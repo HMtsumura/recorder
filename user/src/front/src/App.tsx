906208243d63a04911dabb71c51221b095eb612a
HMtsumura <hironori0811@gmail.com>
2022-04-11 22:50:45 +0900

    dbのrecord一覧取得 ・表示まで確認

--- user/src/front/src/App.tsx
+++ user/src/front/src/App.tsx
@@ -1,33 +1,44 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 
-// const server = 'http://localhost:8088/users';
+const server = 'http://localhost:3000/contents';
 
 interface State{
-  result: string 
+  result: Array<obj>,
+  color_code: String
+}
+
+interface obj{
+  id: Number,
+  title: string,
+  color_code: string,
+  comment: string,
+  record_ymd: string,
 }
 
 class App extends Component {
   state: State ={
-    result: ''
+    result: [],
+    color_code: '',
   }
   constructor(props: any, state: State) {
     super(props);
     this.state = {
-      result: '',
+      result: [],
+      color_code: '',
     };
 
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
-    axios.get('/users')
+    axios.get(server)
       .then((res) => {
         this.setState({
           status: true,
-          result: res.data
+          result: res.data,
+          color_code: res.data[0].color_code
         });
-        console.log(this.state);
       })
       .catch((e) => {
         console.error(e);
@@ -39,11 +50,18 @@ class App extends Component {
   }
 
   render() {
-    const result = <div>{this.state.result}</div>
     return (
       <div>
         <button onClick={this.handleClick}>Get Data</button>
-        {result}
+        {this.state.result.map((elem: obj)=>(
+          <ul>
+            <li>{elem.id}</li>
+            <li>{elem.title}</li>
+            <li style={{backgroundColor: elem.color_code}}>{elem.color_code}</li>
+            <li>{elem.comment}</li>
+            <li>{elem.record_ymd}</li>
+          </ul>
+        ))}
       </div>
     );
   }
