d638ad5da082b7faf26f0ef85a0c5900a7784982
HMtsumura <hironori0811@gmail.com>
2022-04-18 17:06:46 +0900

    登録画面でカテゴリーを選べるよう修正

--- user/src/front/src/App.tsx
+++ user/src/front/src/App.tsx
@@ -11,6 +11,9 @@ import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { Color, ColorPicker, createColor } from 'material-ui-color';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
 import { threadId } from 'worker_threads';
 
 const style = {
@@ -25,7 +28,19 @@ const style = {
   p: 4,
 };
 
-// TODO 変数名考える必要あり
+// TODO メソッド名・変数名考える必要あり
+// TODO 要素のコンポーネント化
+// TODO classじゃなくてexport default functionでreturnで返すようにする
+// TODO タグ選択・作成
+// TODO 登録内容編集・削除
+// TODO　日別の枠出力
+// TODO 日別にデータ分ける
+// TODO 日別でデータ取得
+// TODO ユーザー作成・編集
+// TODO バリデーション
+// TODO デザイン
+// TODO セッション
+// TODO SQLインジェクション対策等
 const server = 'http://localhost:3000/contents';
 const categorizedContents = 'http://localhost:3000/contents/categorized';
 const registContent = 'http://localhost:3000/contents/regist';
@@ -49,10 +64,11 @@ const palette = {
 interface State{
   result: Array<recordObj>,
   categories: Array<categoryObj>,
-  color_code: String,
   open: boolean,
   date: any,
-  color: Color
+  color: Color,
+  selected_category: string,
+  selected_category_id: string
 }
 
 interface recordObj{
@@ -73,36 +89,38 @@ class App extends Component {
   state: State ={
     result: [],
     categories: [],
-    color_code: '',
     open: false,
     date: null,
-    color: createColor("red")
+    color: createColor("red"),
+    selected_category: "",
+    selected_category_id: ""
   }
   constructor(props: any, state: State) {
     super(props);
     this.state = {
       result: [],
       categories: [],
-      color_code: '',
       open: false,
       date: new Date(),
-      color: createColor("red")
+      color: createColor("red"),
+      selected_category: "",
+      selected_category_id: ""
     };
 
     this.handleClick = this.handleClick.bind(this);
     this.handleChange= this.handleChange.bind(this);
     this.handleDateChange= this.handleDateChange.bind(this);
+    this.getAllContents();
   }
 
-  handleClick() {
+  getAllContents(){
     axios.get(server)
       .then((res) => {
         console.log(res.data[1]);
         this.setState({
           status: true,
           result: res.data[0],
-          categories: res.data[1],
-          color_code: res.data[0].color_code
+          categories: res.data[1]
         });
       })
       .catch((e) => {
@@ -112,6 +130,9 @@ class App extends Component {
           result: e,
         });
       });
+  }
+  handleClick() {
+    this.getAllContents();    
   }
 
   handleChange(e: React.ChangeEvent<HTMLSelectElement>){
@@ -144,13 +165,22 @@ class App extends Component {
     this.setState({open: false});
   };
 
-  handleDateChange = (newValue: Date|null)=>{
-    this.setState({date: newValue});
+  handleDateChange = (newDate: Date|null)=>{
+    this.setState({date: newDate});
   }
 
-  handleColorChange = (newValue: Color)=>{
-    console.log(newValue);
-    this.setState({color: newValue});
+  handleColorChange = (newColor: Color)=>{
+    this.setState({color: newColor});
+  }
+
+  handleCategorySelect = (event: SelectChangeEvent, child: any)=>{
+    console.log(child.props.id);
+    this.setState(
+      { 
+        selected_category: event.target.value
+      , selected_category_id: child.props.id
+      }
+    );
   }
 
   handleRegist = (event: React.FormEvent<HTMLFormElement>)=>{
@@ -174,9 +204,11 @@ class App extends Component {
         comment: formData.get('comment'),
         color_code: this.state.color.hex,
         record_ymd: formattedDate,
-        category_id: '1'
+        category_id: this.state.selected_category_id
       }
     }).then((res)=>{
+      this.handleClose();
+      this.getAllContents();
       console.log(res);
     }).catch((e)=>{
       console.error(e);
@@ -213,7 +245,7 @@ class App extends Component {
           aria-describedby="modal-modal-description"
         >
           <Box component="form" onSubmit={this.handleRegist} noValidate sx={ style }>
-            {/* <FormControl> */}
+            <FormControl fullWidth>
               <Typography id="modal-modal-title" variant="h6" component="h2">
                 記録
               </Typography>
@@ -227,6 +259,19 @@ class App extends Component {
                   renderInput={(params: any) => <TextField {...params} />}
                 />
               </LocalizationProvider>
+              <InputLabel id="category-label">Category</InputLabel>
+              <Select
+                labelId="category-label"
+                id="category"
+                name="selected_category"
+                value={this.state.selected_category}
+                label="category"
+                onChange={this.handleCategorySelect}
+              >
+                {this.state.categories.map((category: categoryObj)=>(
+                  <MenuItem id={category.id} value={category.category_name}>{category.category_name}</MenuItem>
+                ))}
+              </Select>
               <ColorPicker
                 value={this.state.color} 
                 onChange={this.handleColorChange}
@@ -259,7 +304,7 @@ class App extends Component {
                   登録
                 </Button>
               </div>
-            {/* </FormControl> */}
+            </FormControl>
           </Box>
         </Modal>
       </div>
